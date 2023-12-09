cf9ac8e9b93869abba694a38c2262c362cdf57ee
kgotsosm <kgotsom@protonmail.com>
2023-12-09 23:40:43 +0200

    Modified method to sendFile

--- myApp.js
+++ myApp.js
@@ -4,8 +4,8 @@ let app = express();
 console.log('Hello World');
 
 app.get("/", function(req, res) {
-    absolutePath = __dirname + "views/index.html"
-    res.send(absolutePath);
+    absolutePath = __dirname + "/views/index.html"
+    res.sendFile(absolutePath);
 });
 
 
