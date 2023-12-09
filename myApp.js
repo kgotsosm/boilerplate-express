8fc28522b75ea0114f0571de322d47164743b591
kgotsosm <kgotsom@protonmail.com>
2023-12-10 01:29:53 +0200

    updated code to render uppercase conditionally

--- myApp.js
+++ myApp.js
@@ -11,16 +11,14 @@ app.get("/", function (req, res) {
 const pubDirectory = __dirname + "/public";
 app.use("/public", express.static(pubDirectory));
 
-app.get("/json", (req, res) => {
-  const mySecret = process.env["MESSAGE_STYLE"];
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    res.json({
-      message: "Hello json".toUpperCase(),
-    });
+const myData = { message: "Hello json" };
+app.get("/json", function (req, res) {
+  const mySecret = process.env.MESSAGE_STYLE;
+
+  if (mySecret === "uppercase") {
+    res.json({ message: "Hello json".toUpperCase() });
   } else {
-    res.json({
-      message: "Hello json",
-    });
+    res.json(myData);
   }
 });
 
