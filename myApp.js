33c150da74b52a80d8449920b58442cc110c820a
kgotsosm <kgotsom@protonmail.com>
2023-12-10 01:12:06 +0200

    New approach

--- myApp.js
+++ myApp.js
@@ -1,61 +1,27 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-require('dotenv').config()
+require("dotenv").config();
 // console.log('Hello World');
 
-app.get("/", function(req, res) {
-    const absolutePath = __dirname + "/views/index.html"
-    res.sendFile(absolutePath);
+app.get("/", function (req, res) {
+  const absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
 });
 
 const pubDirectory = __dirname + "/public";
-app.use('/public', express.static(pubDirectory));
+app.use("/public", express.static(pubDirectory));
 
-const myData = { "message": "Hello json" };
-app.get('/json', function(req, res) {
-    const jsonPath = __dirname + "/json";
-
-    if (process.env.MESSAGE_STYLE == 'uppercase') {
-        myData.message = myData.message.toUpperCase();
-        console.log(myData)
-    }
-
-    res.json(myData);
-    console.log(myData)
+app.get("/json", (req, res) => {
+  const mySecret = process.env["MESSAGE_STYLE"];
+  if (process.env.MESSAGE_STYLE === "uppercase ") {
+    res.json({
+      message: "Hello json".toUpperCase(),
+    });
+  } else {
+    res.json({
+      message: "Hello json",
+    });
+  }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
