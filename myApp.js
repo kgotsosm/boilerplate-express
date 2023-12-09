9e676a71de5b0c3ed0f5a3fc057ccb14df51e077
kgotsosm <kgotsom@protonmail.com>
2023-12-10 00:45:19 +0200

    Fixed code style errors

--- myApp.js
+++ myApp.js
@@ -11,8 +11,8 @@ app.get("/", function(req, res) {
 const pubDirectory = __dirname + "/public";
 app.use('/public', express.static(pubDirectory));
 
+const myData = {"message": "Hello json"};
 app.get("/json", function(req, res) {
-    const myData = {"message": "Hello json"};
 
     if (process.env.MESSAGE_STYLE === 'uppercase') {
         myData.message = myData.message.toUpperCase();
