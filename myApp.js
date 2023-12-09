8ccccc8644da3c2ffb4a57500a172eb9eaf7bebf
kgotsosm <kgotsom@protonmail.com>
2023-12-10 01:05:28 +0200

    updated dotenv import

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-let dotenv = require('dotenv').config()
+require('dotenv').config()
 // console.log('Hello World');
 
 app.get("/", function(req, res) {
@@ -11,9 +11,9 @@ app.get("/", function(req, res) {
 const pubDirectory = __dirname + "/public";
 app.use('/public', express.static(pubDirectory));
 
-const myData = {"message": "Hello json"};
+const myData = { "message": "Hello json" };
 app.get('/json', function(req, res) {
-    const jsonPath = __dirname + "/json"
+    const jsonPath = __dirname + "/json";
 
     if (process.env.MESSAGE_STYLE == 'uppercase') {
         myData.message = myData.message.toUpperCase();
