0c69d5be58dc114874a57b0e77e2adf6c79af8bb
kgotsosm <kgotsom@protonmail.com>
2023-12-10 22:29:33 +0200

    Editing query

--- myApp.js
+++ myApp.js
@@ -53,7 +53,7 @@ app.get("/:word/echo", function(req, res){
 // Get Query Parameter Input from the Client
 app.get("/name", (req, res) => {
   // Create the response JSON document
-  const response = { name: `${req.query}` };
+  const response = { name: `${req.query.firstName}` };
 
   // Send the response
   res.json(response);
